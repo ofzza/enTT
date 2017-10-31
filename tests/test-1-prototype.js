969fe53f87fe8b546460530c631efec957d98d2f
ofzza <ofzza@ofzza.com>
2017-10-31 13:49:27 +0100

    'Forked' from boilerplate

--- tests/test-1-prototype.js
+++ tests/test-1-prototype.js
@@ -1,9 +1,9 @@
 // =====================================================================================================================
-// Dummy test
+// Tests Entity prototype fundamentals
 // =====================================================================================================================
 let assert = require('assert');
 
-describe('Array', () => {
+describe('Entity Prototype', () => {
   describe('#indexOf()', () => {
     it('should return -1 when the value is not present', (done) => {
       assert.equal([1, 2, 3].indexOf(4), -1);
