8647d5bb13a71ee7c68bbb5b6a5f110f61eb72e5
ofzza <ofzza@ofzza.com>
2020-03-05 18:16:40 +0100

    Updated changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,8 @@
 ### 2.0.7
 [x] Added CHANGELOG
 ##### EnTT
-[ ] Cleaned up tests
-[ ] Cleaned up README
+[x] Cleaned up tests
+[ ] Cleaned up README and broken it up into multiple doc files
 [x] Making sure internal EnTT methods implementation doesn't depend on other methods which can be occluded on any EnTT instance + tests
 ##### Dynamic Properties Extension
 [ ] Added support for defining a custom dynamic property setter along with the getter
---
+++ tests/01-entity-base/03-extensions.js
@@ -0,0 +1,12 @@
+// =====================================================================================================================
+// Tests Entity Extensions
+// =====================================================================================================================
+describe('> Entity Extensions', () => {
+
+  // Test dynamic properties extension
+  require('../03-extensions/01-dynamic-properties')();
+
+  // Test validation extension
+  require('../03-extensions/02-validation')();
+
+});
